98d607a171e3ea9fafb75cd0e4e93cd6000eb87b
Christopher Maier <cm@opscode.com>
2013-03-12 18:00:02 -0400

    release v0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cm@opscode.com"
 license          "All rights reserved"
 description      "Installs and configures Pedant test applications"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.2"
 
 # This is one of the new cookbooks:
 # https://github.com/opscode-cookbooks/opscode-ruby
