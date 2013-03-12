d545319790a46fbe8161407283458d398166c619
Christopher Maier <cm@opscode.com>
2013-03-12 17:44:59 -0400

    Comments, dependency bump

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,9 @@ description      "Installs and configures Pedant test applications"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.1"
 
+# This is one of the new cookbooks:
+# https://github.com/opscode-cookbooks/opscode-ruby
+depends "opscode-ruby", "~> 0.1.1"
+
+# This comes from our infrastructure
 depends "git"
-depends "opscode-ruby", "~> 0.1.0"
