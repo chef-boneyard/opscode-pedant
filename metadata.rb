8e006cc39fa54b41ea6778379c3c732289e8f5b8
jtimberman <joshua@opscode.com>
2013-12-18 14:54:57 -0700

    depend on newer opscode-ruby cookbook
    * We're using opscode-ruby 1.2.0 in our infrastructure

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email "cm@opscode.com"
 license          "All rights reserved"
 description      "Installs and configures Pedant test applications"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.2"
+version          "0.2.0"
 
 # This is one of the new cookbooks:
 # https://github.com/opscode-cookbooks/opscode-ruby
-depends "opscode-ruby", "~> 0.1.1"
+depends "opscode-ruby", "~> 1.2"
 
 # This comes from our infrastructure
 depends "git"
